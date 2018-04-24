073a06e552596838f8a94bdb7144a9ccb85e24fc
DragonEx <service@dragonex.io>
2018-04-24 11:33:20 +0800

    golang example

---
+++ examples/golang/sign.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"sort"
+	"strings"
+)
+
+const (
+	AccessKey = "ThisIsAccessKey"
+	SecretKey = "ThisIsSecretKey"
+)
+
+func main() {
+	method := "POST"
+	headers := map[string]string{
+		"Date":            "Mon, 1 Jan 2018 08:08:08 GMT",
+		"Content-MD5":     "123abc",
+		"Content-Type":    "application/json",
+		"dragonex-btruth": "DragonExIsTheBest2",
+		"Dragonex-Atruth": "DragonExIsTheBest",
+	}
+	resource := "/api/v1/token/new/"
+	sign(SecretKey, method, headers, resource)
+
+}
+
+var (
+	SignError = errors.New("sign error")
+)
+
+func sign(secretKey, method string, headers map[string]string, resource string) (string, error) {
+	newHeaders := map[string]string{}
+	for k, v := range headers {
+		newHeaders[strings.ToLower(k)] = v
+	}
+
+	contentMd5 := newHeaders["content-md5"]
+	contentType := newHeaders["content-type"]
+	date := newHeaders["date"]
+
+	dragonHeaders := make([]string, 0, len(newHeaders))
+	canonicalizedDragonExHeaders := ""
+	for k, v := range newHeaders {
+		if strings.HasPrefix(k, "dragonex-") {
+			dragonHeaders = append(dragonHeaders, fmt.Sprintf("%s:%s", k, v))
+		}
+	}
+	sort.Strings(dragonHeaders)
+	if len(dragonHeaders) != 0 {
+		canonicalizedDragonExHeaders = strings.Join(dragonHeaders, "\n")
+		canonicalizedDragonExHeaders += "\n"
+	}
+
+	stringsToSignSlice := []string{
+		strings.ToUpper(method),
+		contentMd5,
+		contentType,
+		date,
+		canonicalizedDragonExHeaders,
+	}
+	stringToSign := strings.Join(stringsToSignSlice, "\n")
+	stringToSign += resource
+
+	sha1Hash := hmac.New(sha1.New, []byte(secretKey))
+	if _, e := sha1Hash.Write([]byte(stringToSign)); e != nil {
+		return "", SignError
+	}
+
+	signature := base64.StdEncoding.EncodeToString(sha1Hash.Sum(nil))
+
+	println(fmt.Sprintf("stringToSign=%s\nsign=%s", stringToSign, signature))
+	return signature, nil
+}
